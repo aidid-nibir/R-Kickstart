80a995beafb2f570a45a6edc7b17cf79b9b81278
aidid-nibir <aidid.nibir97@gmail.com>
2022-03-30 23:30:12 +0600

    modified name generator

--- userNameGenerator.R
+++ userNameGenerator.R
@@ -1,6 +1,6 @@
 {
-  first_name = "Neelima"
-  last_name = "Jaman"
-  year_of_birth = "1999"
+  first_name = "Aidid"
+  last_name = "Nibir"
+  year_of_birth = "1994"
   print(paste("Your user id: ",first_name,last_name,year_of_birth, sep = ""))
 }
